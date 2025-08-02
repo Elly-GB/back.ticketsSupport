1a8481bc195fadc4ba56760c8e8bebb0c457325b
Gaby <gabyfranbublitz@gmail.com>
2025-08-02 09:49:41 -0300

    added node:crypto ID

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -1,7 +1,10 @@
+import { randomUUID } from "node:crypto"
+
 export function create({ request, response }) {
   const { equipment, description, user_name } = request.body
 
   const ticket = {
+    id: randomUUID(),
     equipment,
     description,
     user_name,
